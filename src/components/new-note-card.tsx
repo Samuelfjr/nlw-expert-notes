a26ff231ad97c8508e236a785f96754559ba1b64
Samuelfjr <samucaftww@gmail.com>
2024-02-08 17:07:20 -0300

    feat: Reseta Nota

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -28,6 +28,9 @@ export function NewNoteCart({ onNoteCreated }: NewNoteCartProps) {
 
     onNoteCreated(content);
 
+    setContent("");
+    setShouldShowOnboarding(true);
+
     toast.success("Nota criada com sucesso!");
   }
 
@@ -74,6 +77,7 @@ export function NewNoteCart({ onNoteCreated }: NewNoteCartProps) {
                   autoFocus
                   className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                   onChange={handleContentChange}
+                  value={content}
                 />
               )}
             </div>
