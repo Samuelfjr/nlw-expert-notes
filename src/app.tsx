cbe6db2f1d4d8678f76d0e4d4b6fd341ba371462
Samuelfjr <samucaftww@gmail.com>
2024-02-08 13:10:21 -0300

    feat: Props Cartão

--- src/app.tsx
+++ src/app.tsx
@@ -4,14 +4,14 @@ import { NoteCard } from "./components/note-card";
 
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="max-w-6xl mx-auto my-12 space-y-6">
       <img src={logo} alt="NLW Experts" />
 
       <form className="w-full">
         <input
           type="text"
           placeholder="Busque em suas notas..."
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+          className="w-full text-3xl font-semibold tracking-tight bg-transparent outline-none placeholder:text-slate-500"
         />
       </form>
 
@@ -19,11 +19,12 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCart />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard
+          note={{
+            date: new Date(),
+            content: "Hello World",
+          }}
+        />
       </div>
     </div>
   );
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,16 +1,29 @@
-export function NoteCard() {
+import * as Dialog from "@radix-ui/react-dialog";
+
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">Há 2 dias.</span>
+    <Dialog.Root>
+      <Dialog.Trigger className="relative flex flex-col gap-3 p-5 overflow-hidden text-left rounded-md outline-none bg-slate-800 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
 
-      <p className="text-sm leading-6 text-slate-400 ">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit,
-        voluptatibus neque eius facilis vel reprehenderit hic voluptas commodi,
-        sunt quod ipsam dolore veniam quidem distinctio, aspernatur ullam
-        ratione accusantium nisi?
-      </p>
+        <p className="text-sm leading-6 text-slate-400 ">{note.content}</p>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0  pointer-events-none" />
-    </button>
+        <div className="absolute bottom-0 left-0 right-0 pointer-events-none h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="z-10 absolute">oi</Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
